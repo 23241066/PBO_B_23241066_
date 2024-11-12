f33e9df030a0f94d978f42f000a07f4b16d746b4
MSyakirArridho <146050299+23241066@users.noreply.github.com>
2024-11-12 14:50:57 +0800

    12 NOV 2024

---
+++ OPP - B - MODUL 3/08 - TPM - 03/src/App.java
@@ -0,0 +1,17 @@
+class Hitung_Gaji_pokok {
+    // data member
+    private int gaji_pokok;
+    private int tunjangan;
+    private double pajak;
+
+    // constructor
+    private Hitung_Gaji_pokok() {
+        this.intgaji_pokok = 0;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
